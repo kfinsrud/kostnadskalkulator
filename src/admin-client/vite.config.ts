7bfa017d5249392b3205bf1179582b198287773d
andrrma <andrrma@stud.ntnu.no>
2024-06-24 14:11:10 +0200

    Adds support for ckeditor5 editor builds from source. Inlines ckeditor to prevent .css format error related to loading ckeditor theme.

--- src/admin-client/vite.config.ts
+++ src/admin-client/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
-
+import ckeditor5 from '@ckeditor/vite-plugin-ckeditor5';
 // https://vitejs.dev/config/
 export default defineConfig({
     build: {
@@ -19,6 +19,13 @@ export default defineConfig({
         coverage: {
             provider: 'v8'
         },
+        server: {
+            deps: {
+                inline: [
+                    /@ckeditor\/.*/i, /ckeditor5\/.*/i
+                ]
+            }
+        }
     },
     plugins: [
         react(),
@@ -26,6 +33,7 @@ export default defineConfig({
         svgr({
             include: '**/*.svg?react',
         }),
+        ckeditor5( { theme: require.resolve( '@ckeditor/ckeditor5-theme-lark' ) } ),
     ],
 });
 //    "test": "jest --runInBand --detectOpenHandles --forceExit --collect-coverage",
