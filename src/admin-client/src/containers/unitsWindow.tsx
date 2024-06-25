bb805c2fa771fd4367e221eec11c728f5ece1423
andrrma <andrrma@stud.ntnu.no>
2024-06-25 13:37:33 +0200

    Updates units panel to use new editor

--- src/admin-client/src/containers/unitsWindow.tsx
+++ src/admin-client/src/containers/unitsWindow.tsx
@@ -1,5 +1,5 @@
 import Container from "react-bootstrap/Container";
-import {InputGroup, Row} from "react-bootstrap";
+import {Col, InputGroup, Row} from "react-bootstrap";
 import {useAppDispatch, useAppSelector} from "../state/hooks";
 import {TextEditor} from "../components/input/textEditor";
 import {selectUnits} from "../state/store";
@@ -8,34 +8,41 @@ import parse from 'html-react-parser';
 import Button from "react-bootstrap/Button";
 import {SlArrowDown, SlArrowUp} from "react-icons/sl";
 import {useState} from "react";
+import {CKEditorUnits} from "../components/input/CKEditorUnits";
 
 export function UnitBox(props: {
     unit: Unit,
     onChange: (newName: string) => void,
     onMove: (newIndex: number) => void,
-    onDelete: () => void
+    onDelete: () => void,
+    totalUnits: number
 }) {
     const [unitName, setUnitName] = useState(props.unit.name);
     return <Row>
-        <InputGroup>
+        <Col sm={4}>
             {parse(unitName)}
-            <Button onClick={() => {
-                props.onMove(props.unit.ordering - 1)
-            }}><SlArrowUp/></Button>
-            <Button onClick={() => {
-                props.onMove(props.unit.ordering + 1)
-            }}><SlArrowDown/></Button>
-            <Button onClick={() => {
-                props.onDelete()
-            }}>X</Button>
-            <TextEditor
-                value={unitName}
-                buttonText={"Edit"}
-                onSave={(newName: string) => {
-                    props.onChange(newName)
-                    setUnitName(newName)
-                }}/>
-        </InputGroup>
+        </Col>
+        <Col sm={8}>
+            <InputGroup>
+                <Button
+                    disabled={ props.unit.ordering == 0 }
+                    onClick={() => {
+                    props.onMove(props.unit.ordering - 1)
+                }}><SlArrowUp/></Button>
+                <Button disabled={ props.unit.ordering == props.totalUnits-1 } onClick={() => {
+                    props.onMove(props.unit.ordering + 1)
+                }}><SlArrowDown/></Button>
+                <Button onClick={() => {
+                    props.onDelete()
+                }}>X</Button>
+                <CKEditorUnits
+                    value={unitName}
+                    onSave={(newName: string) => {
+                        props.onChange(newName)
+                        setUnitName(newName)
+                    }}/>
+            </InputGroup>
+        </Col>
     </Row>
 }
 
@@ -46,19 +53,23 @@ export function UnitsWindow() {
     return <>
         <Container style={{minHeight: "100%", overflowY:"scroll", maxHeight:"100%"}}>
             <Row>
-                <TextEditor
-                    value={""}
-                    buttonText={"Add Unit"}
-                    onSave={(value: string) => {
-                        dispatch(addUnit({name: value, ordering: 1}))
-                    }}/>
+                <Col>
+                    <Button
+                        onClick={()=>{
+                            dispatch(addUnit({name: "", ordering: 0}))
+                        }}
+                    >
+                        Add new unit
+                    </Button>
+                </Col>
             </Row>
             {units.map(({id, unit}) => {
                 return <Row key={id}>
                     <UnitBox
+                        totalUnits={units.length}
                         unit={unit}
                         onChange={(newName) => {
-                            updateUnit({...unit, name: newName})
+                            dispatch(updateUnit({...unit, name: newName}))
                         }}
                         onMove={(newIndex: number) => {
                             dispatch(moveUnit({oldIndex: unit.ordering, newIndex: newIndex}))
