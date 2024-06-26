edda7dade98d81409dd6ac5759216817b4ed0b18
andrrma <andrrma@stud.ntnu.no>
2024-06-26 10:26:05 +0200

    Alters text of StartPage when in developer mode to make it clearly distinct from regular mode

--- src/client/src/pages/StartPage.tsx
+++ src/client/src/pages/StartPage.tsx
@@ -10,18 +10,19 @@ export function StartPage(
                 <Card.Body>
                     <Row className={"mx-auto"} style={{maxWidth: '800px'}}>
                         <Col xs={12} md={8}>
-                            <h1 style={{fontWeight: 700, fontSize: '40px'}}>Beregn produktivitet og kostnader ved
-                                skogsdrift
+                            <h1 style={{fontWeight: 700, fontSize: '40px'}}>{props.developerMode? ""  : "Beregn produktivitet og kostnader ved skogsdrift"}
                             </h1>
                             <Row className={"mt-4 mb-4"} style={{fontWeight: 500}}>
                                 <Col>
                                     <Row className={"mt-2 mb-4"} style={{fontSize: '18px'}}>
-                                        <p>{"Kalkulatoren gir en prognose på tidsbruk og kostnader basert på skogtype, driftsforhold og hvordan drifta er tilrettelagt."}</p>
+                                        {props.developerMode? <p>{"OBS! Dette er forhåndsvisning av kalkulatorer som er under utvikling. Disse bør ikke tas i bruk."}</p> :
+                                        <p>{"Kalkulatoren gir en prognose på tidsbruk og kostnader basert på skogtype, driftsforhold og hvordan drifta er tilrettelagt."}</p>}
                                     </Row>
                                     <Row className={"mb-5"}>
                                         <CalculatorPicker developerMode={props.developerMode}/>
                                     </Row>
                                     <hr style={{color: "darkgray"}}/>
+                                    { !props.developerMode &&
                                     <i>
                                         <Row className={"pt-4 mb-2"}>
                                             <p>{"Kalkulatoren er utviklet av Skogkurs i samarbeid med NIBIO, Allskog, Glommen Mjøsen Skog og MEF-Skog. Programmeringen er utført av en gruppe bachelor-studenter ved NTNU i Gjøvik."}</p>
@@ -29,7 +30,7 @@ export function StartPage(
                                         <Row>
                                             <p>{"Kalkulatoren er finansiert med støtte fra Skogbrukets Utviklingsfond og Skogbrukets Verdiskapingsfond."}</p>
                                         </Row>
-                                    </i>
+                                    </i>}
                                 </Col>
                             </Row>
                         </Col>
